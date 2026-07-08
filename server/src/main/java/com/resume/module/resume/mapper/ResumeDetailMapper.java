ae6d433194607c0e5fce662fa2e7beb0d935972f
田梦婷 <2234786271@qq.com>
2026-07-08 11:13:42 +0800

    feat(resume): 简历导入导出模块 - 数据库设计、CRUD接口、文件存储、OCR集成(qwen3.5-ocr)

---
+++ server/src/main/java/com/resume/module/resume/mapper/ResumeDetailMapper.java
@@ -0,0 +1,7 @@
+﻿package com.resume.module.resume.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.resume.module.resume.entity.ResumeDetail;
+
+public interface ResumeDetailMapper extends BaseMapper<ResumeDetail> {
+}
