0ac83241ad934ad2fc2b73eddef35bbc077da54c
wangcan <wangconcker@gmail.com>
2026-07-08 16:12:49 +0800

    feat(邮箱验证码发送接口成功跑通)

--- server/src/main/java/com/resume/module/resume/mapper/ResumeDetailMapper.java
+++
@@ -1,7 +0,0 @@
-﻿package com.resume.module.resume.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.resume.module.resume.entity.ResumeDetail;
-
-public interface ResumeDetailMapper extends BaseMapper<ResumeDetail> {
-}
--- server/src/main/java/com/resume/module/resume/mapper/ResumeFileMapper.java
+++
@@ -1,7 +0,0 @@
-﻿package com.resume.module.resume.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.resume.module.resume.entity.ResumeFile;
-
-public interface ResumeFileMapper extends BaseMapper<ResumeFile> {
-}
